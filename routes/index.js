98a53ccfa6d42afa9e97f55c6ab6010eea12139b
Shivam Kumar <shivam.saha06@gmail.com>
2020-08-17 23:09:25 +0530

    Api Created

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+const Url = require('../models/Url');
+
+// @route GET /:code
+// @desc Redirect to long/original Url
+
+router.get('/:code', async (req, res) => {
+     try {
+         const url = await Url.findOne({ urlCode: req.params.code });
+
+         if(url) {
+            return res.redirect(url.longUrl);
+         } else {
+             return res.status(404).json('No Url found');
+         }
+     } catch (err) { 
+         console.error(err);
+         res.send(500).json('Server Error');
+     }
+});
+
+module.exports = router;
